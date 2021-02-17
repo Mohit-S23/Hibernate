f1a47a7ea5c85b1db887626aac1b22648c712402
Mohit-S23 <sharma.mohit0323@gmail.com>
2021-02-17 22:28:55 +0530

    Advance Mappings: @OneToMany

---
+++ hb-03-one-to-many/src/com/mohit/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,60 @@
+package com.mohit.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mohit.hibernate.demo.entity.Course;
+import com.mohit.hibernate.demo.entity.Instructor;
+import com.mohit.hibernate.demo.entity.InstructorDetail;
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create the objects
+			
+			Instructor tempInstructor = new Instructor("Chandler", "Bing", "bing.chandler@gmail.com");
+			
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube.com", "Sarcasm");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			//
+			// Note: this will also save the details object because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);
+						
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
