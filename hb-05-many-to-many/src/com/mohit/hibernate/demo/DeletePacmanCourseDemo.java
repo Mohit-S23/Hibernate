9f4108d87f5e2811da94156e23b1b32c1b8e547f
Mohit-S23 <sharma.mohit0323@gmail.com>
2021-02-19 12:04:02 +0530

    Advance Mappings: @ManyToMany

---
+++ hb-05-many-to-many/src/com/mohit/hibernate/demo/DeletePacmanCourseDemo.java
@@ -0,0 +1,58 @@
+package com.mohit.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mohit.hibernate.demo.entity.Course;
+import com.mohit.hibernate.demo.entity.Instructor;
+import com.mohit.hibernate.demo.entity.InstructorDetail;
+import com.mohit.hibernate.demo.entity.Review;
+import com.mohit.hibernate.demo.entity.Student;
+
+public class DeletePacmanCourseDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+						
+			// get the Pacman course from db
+			int courseId = 10;
+			Course tempCourse = session.get(Course.class, courseId);
+			
+			// delete the course
+			System.out.println("Deleting course: " + tempCourse);
+			
+			session.delete(tempCourse);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
